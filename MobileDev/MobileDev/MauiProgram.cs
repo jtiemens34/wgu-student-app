80176763a3c5b74383e00bc0bd56c79cb2741d91
elegantlytragic <zomg.its.jesse@hotmail.com>
2023-11-10 16:14:39 -0800

    Add Local Notification Setup to MauiProgram.cs.

--- MobileDev/MobileDev/MauiProgram.cs
+++ MobileDev/MobileDev/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Plugin.LocalNotification;
 using SQLitePCL;
 
 namespace MobileDev;
@@ -14,7 +15,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.UseLocalNotification();
 
 #if DEBUG
 		builder.Logging.AddDebug();
